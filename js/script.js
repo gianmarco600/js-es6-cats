571e951d4fe8fe1e3b97eeb53ec1d66c3215be33
gianmarco <gianmarco.palombo600@gmail.com>
2021-06-17 17:48:13 +0200

    fine

--- js/script.js
+++ js/script.js
@@ -4,40 +4,121 @@
 const gatti = [
     {
         nome: 'Duchessa',
-        eta: 4,
+        eta: 7,
         colore: 'bianco',
-        sesso: 'f'
+        genere: 'f'
     },
     {
         nome: 'Romeo',
-        eta: 4,
+        eta: 10,
         colore: 'arancione',
-        sesso: 'm'
+        genere: 'm'
     },
     {
         nome: 'Minou',
-        eta: 4,
+        eta: 2,
         colore: 'bianco',
-        sesso: 'f'
+        genere: 'f'
     },
     {
         nome: 'Matisse',
-        eta: 4,
+        eta: 2,
         colore: 'rosso',
-        sesso: 'm'
+        genere: 'm'
     },
     {
         nome: 'Bizet',
-        eta: 4,
+        eta: 2,
         colore: 'grigio',
-        sesso: 'm'
+        genere: 'm'
+    },
+    {
+        nome: 'Hit',
+        eta: 7,
+        colore: 'yellow',
+        genere: 'm'
     }
 ];
 document.getElementById('gatti').innerHTML = '';
 
 gatti.forEach((gatto) => { 
-    let {nome , colore} = gatto;
+    let {nome , colore , genere} = gatto;
+    document.getElementById('gatti').innerHTML+= `<li> <i class="fas fa-cat ${colore} "></i> ${nome} è un gatto ${colore} </li>`;
     
-    document.getElementById('gatti').innerHTML+= '<li> <i class="fas fa-cat '+ colore +'"></i>   ' +  nome + " è un gatto" + colore + ' </li>';
+ } );
+
+const pink = '#EEA7B3';
+const blu = '#211CBB';
+
+
+const newCats = gatti.map((gatto) =>{
+    const nome = gatto.nome;
+    const eta = gatto.eta;
+    const colore = gatto.colore;
+    const genere = gatto.genere;
+    const coloreFiocco = (genere == "f")? pink : blu;
+    const opacity = eta / 10;
+
+    return {
+        nome ,
+        eta ,
+        colore , 
+        genere ,
+        fiocco :{
+            colore : coloreFiocco,
+            opacity
+        }
+    }
+});
+
+// newCats.forEach((cat) => {
+//     document.getElementById('gatti_f').innerHTML+= `<li> ${cat.nome} <i class="fas fa-ribbon ${cat.genere} " style=opacity:${cat.fiocco.opacity}></i></li>`;
+// });
+
+const maleCats = newCats.filter((element) => {
+    return element.genere === "m";
+
+});
+
+maleCats.forEach((cat) => {
+    document.getElementById('gatti_m').innerHTML+= `<li> ${cat.nome} <i class="fas fa-ribbon ${cat.genere} " style=opacity:${cat.fiocco.opacity}></i></li>`;
+    console.log(cat.fiocco.colore);
+});
+
+const femaleCats = newCats.filter((element) => {
+    return element.genere === "f";
+
+});
+
+femaleCats.forEach((cat) => {
+    document.getElementById('gatti_f').innerHTML+= `<li> ${cat.nome} <i class="fas fa-ribbon ${cat.genere} " style=opacity:${cat.fiocco.opacity}></i></li>`;
     
- } )
+});
+
+// gatti.forEach((gatto) => { 
+//     let {nome , colore , genere} = gatto;
+//     if (genere == 'f'){
+//         document.getElementById('gatti_f').innerHTML+= `<li class=" ${genere} "> <i class="fas fa-cat ${colore} "></i> ${nome} è un gatto ${colore} </li>`;
+//     }
+//     else{
+//         document.getElementById('gatti_m').innerHTML+= `<li class=" ${genere} "> <i class="fas fa-cat ${colore} "></i> ${nome} è un gatto ${colore} </li>`;
+//     }
+    
+// } );
+
+let catsFemaleMale = [...femaleCats , ...maleCats];
+const catsFemaleMaleSemp = catsFemaleMale.map((element) => {
+    const{nome , colore , fiocco} = element;
+    return{
+        nome,
+        colore,
+        fiocco
+    }
+
+});
+
+catsFemaleMaleSemp.forEach((cat) => {
+    document.getElementById('gatti_ordinati').innerHTML+= `<li> nome: ${cat.nome} <i class="fas fa-ribbon" style='color:${cat.fiocco.colore}; opacity:${cat.fiocco.opacity} '></i> </li>`;
+});
+
+
